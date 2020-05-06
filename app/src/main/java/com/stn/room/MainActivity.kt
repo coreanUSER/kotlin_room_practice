7211c7a07917e8f326c4903f4338762aac3bef2b
gf61 <ghnam@goodsflow.com>
2020-05-06 17:52:40 +0900

    Start Kotlin Room Practice MainActivity - FAB onClickListener 2020-05-06 17:50 By me

--- app/src/main/java/com/stn/room/MainActivity.kt
+++ app/src/main/java/com/stn/room/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.stn.room
 
+import android.content.pm.ApplicationInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.android.material.bottomappbar.BottomAppBar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // BottomAppBar Binding
+        val bottomAppBar: BottomAppBar = findViewById(R.id.bar)
+        setSupportActionBar(bottomAppBar)
+
+        fab.setOnClickListener {
+            Toast.makeText(applicationContext, "클릭", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
